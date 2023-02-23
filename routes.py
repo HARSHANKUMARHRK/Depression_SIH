0bd64a9c429fb56a470af7c9a19f9269c15b4803
snekha <snekhasuresh211003@gmail.com>
2023-02-23 11:20:29 +0530

    updates

--- routes.py
+++ routes.py
@@ -191,7 +191,7 @@ def mri():
       return render_template("result.html", html_data = html_data , result=result, cmt=cmt)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=8000,debug=True)
+    app.run(host="0.0.0.0",port=8080,debug=True)
 
 
     
